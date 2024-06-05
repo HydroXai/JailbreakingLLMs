5de9fc649f485ccb622e67b85b4bd27b900803bd
yasu52 <tsuguro4649@gmail.com>
2024-06-05 10:30:37 -0700

    Add setup commands

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            return line.split("'")[1]
+
+    raise RuntimeError('Unable to find version string.')
+
+
+with open('requirements.txt', 'r') as requirements:
+    setup(name='jailbreaking_llms',
+          version=get_version('jailbreaking_llms/__init__.py'),
+          install_requires=list(requirements.read().splitlines()),
+          packages=find_packages(),
+          description='library for creating adversarial prompts using PAIR for language models',
+          python_requires='>=3.6',
+          author='Patrick Chao and Alexander Robey and Edgar Dobriban and Hamed Hassani and George J. Pappas and Eric Wong',
+          classifiers=[
+              'Programming Language :: Python :: 3',
+              'License :: OSI Approved :: MIT License',
+              'Operating System :: OS Independent'
+          ],
+          long_description=long_description,
+          long_description_content_type='text/markdown')
